d69e247cba06f68f05e299c6808ba93d5e20c51e
Uday Manchanda <uday.manchanda14@gmail.com>
2021-01-16 04:22:22 -0500

    finished exercise 3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -99,7 +101,7 @@ app.get('/info', (req, res) => {
   )
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
